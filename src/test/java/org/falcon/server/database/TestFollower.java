8d119b8c55c499f3753ed44b6d87b146e9e0daeb
Buttigieg Chloe <chloe.buttigieg12@gmail.com>
2023-03-09 09:34:17 +0100

    add folower test

--- src/test/java/org/falcon/server/database/TestFollower.java
+++ src/test/java/org/falcon/server/database/TestFollower.java
@@ -1,4 +1,14 @@
 package org.falcon.server.database;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
 public class TestFollower {
+    @Test
+    public void testGetFollower() throws SQLException {
+        //TODO add utser and check follower
+        //Follower follower = new Follower();
+    }
 }
